a871a9a478ac452a97b08fff165d8280361a90af
dongju <judong1214@gmail.com>
2023-12-20 02:44:48 -0500

    pls

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <NavLink exact to="/" activeClassName="Navbar-active">Home</NavLink>
             <NavLink exact to="/about" activeClassName="Navbar-active">About</NavLink>
             <NavLink exact to="/projects" activeClassName="Navbar-active">Projects</NavLink>
-            <a href="./Website/Julians_Resume_2023_2024.pdf" className="Navbar-link">Resume</a>
+            <a href="./Julians_Resume_2023_2024.pdf" className="Navbar-link">Resume</a>
         </nav>
     )
 }
