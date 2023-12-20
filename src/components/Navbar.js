b8843a6735b4a9d9ef8c9a7b8f01b14a417851d3
dongju <judong1214@gmail.com>
2023-12-20 03:35:09 -0500

    finally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <nav className="Navbar">
-            <NavLink exact to="/" activeClassName="Navbar-active">Home</NavLink>
-            <NavLink exact to="/about" activeClassName="Navbar-active">About</NavLink>
-            <NavLink exact to="/projects" activeClassName="Navbar-active">Projects</NavLink>
-            <a href="./Julians_Resume_2023_2024.pdf" className="Navbar-link">Resume</a>
+            <NavLink to="/Website" activeClassName="Navbar-active" end>Home</NavLink>
+            <NavLink to="/Website/about" activeClassName="Navbar-active">About</NavLink>
+            <NavLink to="/Website/projects" activeClassName="Navbar-active">Projects</NavLink>
+            <a href="https://judong1214.github.io/Website/Julians_Resume_2023_2024.pdf" className="Navbar-link" rel="noopener noreferrer">Resume</a>
         </nav>
     )
 }
